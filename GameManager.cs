e105c938a36e65b994195420f146c26f24da103e
Mohammed Shareef <mohammed.shareef@hotmail.com>
2024-07-28 11:02:20 +0100

    Added a class to manage the game flow

---
+++ GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper
+{
+    /// <summary>
+    /// Creates and controls the entire game flow
+    /// </summary>
+    public class GameManager
+    {
+        public GameManager(int boardSize, int mineCount)
+        {
+
+        }
+    }
+}
