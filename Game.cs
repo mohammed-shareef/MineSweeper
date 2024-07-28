6c8dc2159b4cd2ae1d94894aa6d77d53054ec53e
Mohammed Shareef <mohammed.shareef@hotmail.com>
2024-07-28 11:30:37 +0100

    Initial version of a Game instance

---
+++ Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper
+{
+    /// <summary>
+    /// Manage's an instance of the game currently in play
+    /// </summary>
+    public class Game
+    {
+        private readonly GameBoard _board;
+
+        public Game(int boardSize, int mineCount)
+        {
+            _board = new GameBoard(boardSize, mineCount);
+        }
+    }
+}
