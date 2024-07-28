b78c73a27c2a82790fbc748db65af08ecc26f534
Mohammed Shareef <mohammed.shareef@hotmail.com>
2024-07-28 11:18:02 +0100

    Placing mines on the game board

--- GameBoard.cs
+++ GameBoard.cs
@@ -8,6 +8,30 @@ namespace MineSweeper
 {
     public class GameBoard
     {
+        private readonly bool[,] _mines;
 
+        public GameBoard(int size, int mineCount)
+        {
+            _mines = new bool[size, size];
+            PlaceMines(mineCount);
+        }
+
+        private void PlaceMines(int mineCount)
+        {
+            var random = new Random();
+            var placedMines = 0;
+
+            while (placedMines < mineCount)
+            {
+                int x = random.Next(_mines.GetLength(0));
+                int y = random.Next(_mines.GetLength(1));
+
+                if (!_mines[x, y])
+                {
+                    _mines[x, y] = true;
+                    placedMines++;
+                }
+            }
+        }
     }
 }
