f082ec484d4e3d3d3d26d347b0e7d41da74ee4aa
Mohammed Shareef <mohammed.shareef@hotmail.com>
2024-07-28 11:33:59 +0100

    Initial output display

--- GameManager.cs
+++ GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace MineSweeper
 {
@@ -11,8 +12,22 @@ namespace MineSweeper
     /// </summary>
     public class GameManager
     {
+        private readonly Game _game;
+
         public GameManager(int boardSize, int mineCount)
         {
+            _game = new Game(boardSize, mineCount);
+        }
+
+        public void Run()
+        {
+            Console.Clear();
+            Console.WriteLine("******************** Mine Sweeper  ********************");
+            Console.WriteLine();
+            Console.WriteLine("Enter direction to move ");
+            Console.WriteLine("[Up - u, Down - d, Left - l, Right - r] or q to quit :");
+            Console.WriteLine();
+            Console.WriteLine("******************************************************");
 
         }
     }
